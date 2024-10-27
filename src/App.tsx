244418d154eb07e114a6f27f2d83ac04e8c24833
Lucas Ribeiro Bonfilio de Lemos <lucasrblemos@gmail.com>
2024-10-27 02:43:53 -0300

    feat: create project files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div className='bg-green-500'></div>
+    </div>
+  );
+}
+
+export default App;
